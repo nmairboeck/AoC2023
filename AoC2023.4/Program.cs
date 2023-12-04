eec3735105d4c3284d7bc2a093b116629d61de4d
n.mairboeck@countit.at <n.mairboeck@gmail.com>
2023-12-04 18:56:19 +0100

    Day 4 part 2

--- AoC2023.4/Program.cs
+++ AoC2023.4/Program.cs
@@ -1,25 +1,69 @@
 ﻿
 
+using System.Diagnostics;
+
 var lines = await File.ReadAllLinesAsync("Input.txt");
 
-var cards = lines.Select(l => l.Substring(l.IndexOf(':') + 2).Split('|', StringSplitOptions.RemoveEmptyEntries)).ToList();
 
+var cards = lines
+    .Select(l => l[(l.IndexOf(':') + 2)..].Split('|', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()).ToArray())
+    .ToList();
+
+var stopwatch = new Stopwatch();
+
+stopwatch.Start();
 var sumPoints = 0;
 foreach (var card in cards)
 {
     sumPoints += CalculateWinningPoints(card);
 }
-static int CalculateWinningPoints(string[] card)
+static int CalculateWinningPoints(int[][] card)
 {
-    var winningNumbers = card[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
-    var havingNumbers = card[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+    var winningNumbers = card[0];
+    var havingNumbers = card[1];
 
-    var winningCount = havingNumbers.Where(n => winningNumbers.Contains(n)).Count();
+    var winningCount = havingNumbers.Count(n => winningNumbers.Contains(n));
 
     return (int)Math.Pow(2, winningCount - 1); //pow -1 is 0.5, cast to int is 0    
 }
 
+stopwatch.Stop();
+Console.WriteLine($"The sum of points is: {sumPoints}. Part 1 took {stopwatch.Elapsed}.");
 
-Console.WriteLine($"The sum of points is: {sumPoints}");
+stopwatch.Reset();
+stopwatch.Start();
 
-var pointsPart2 = 0;
+var sumCards = 0;
+Dictionary<int, int> winningCountsPerCard = [];
+
+for (var i = 0; i < cards.Count; i++)
+{
+    sumCards += CalculateWinningPointsRecursive(cards, i, winningCountsPerCard);
+}
+
+
+static int CalculateWinningPointsRecursive(List<int[][]> cards, int start, Dictionary<int, int> winningCountsPerCard)
+{
+    var sum = 0;
+    sum++;
+    var card = cards[start];
+    var winningNumbers = card[0];
+    var havingNumbers = card[1];
+
+    if (!winningCountsPerCard.TryGetValue(start, out var winningCount))
+    {
+        winningCount = havingNumbers.Count(winningNumbers.Contains);
+        winningCountsPerCard[start] = winningCount;
+    }
+
+    for (var j = 1; j <= winningCount; j++)
+    {
+        sum += CalculateWinningPointsRecursive(cards, start + j, winningCountsPerCard);
+    }
+
+
+    return sum;
+}
+
+stopwatch.Stop();
+Console.WriteLine($"The sum of total cards is {sumCards}. Part 2 took {stopwatch.Elapsed}");
